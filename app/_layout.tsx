c197133f0b4199996547ca1f69b26e019df18686
PROGRAMADOR EXTERNO <programador_externo@zermat.com.mx>
2025-10-02 11:11:08 -0600

    feat: update pnpm

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 import "../global.css";
 
 import SplashScreen from "@/components/SplashScreen";
@@ -19,38 +21,11 @@ export default function RootLayout() {
 
    return (
       <>
-         <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-         </Stack>
+         <SafeAreaProvider>
+            <Stack>
+               <Stack.Screen name="index" options={{ headerShown: false }} />
+            </Stack>
+         </SafeAreaProvider>
       </>
    );
 }
-
-// import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
-// import { useFonts } from "expo-font";
-// import { Stack } from "expo-router";
-// import { StatusBar } from "expo-status-bar";
-// import "react-native-reanimated";
-
-// import { useColorScheme } from "@/hooks/useColorScheme";
-
-// export default function RootLayout() {
-//    const colorScheme = useColorScheme();
-//    const [loaded] = useFonts({
-//       SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-//    });
-
-//    if (!loaded) {
-//       return null;
-//    }
-
-//    return (
-//       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-//          <Stack>
-//             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-//             <Stack.Screen name="+not-found" />
-//          </Stack>
-//          <StatusBar style="auto" />
-//       </ThemeProvider>
-//    );
-// }
